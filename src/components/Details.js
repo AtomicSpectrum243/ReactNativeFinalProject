e18ba87bb307d8880bd85566dcd01d5455e315a0
AtomicSpectrum243 <7523494@gmail.com>
2022-10-12 03:28:42 +0700

    first commit

---
+++ src/components/Details.js
@@ -0,0 +1,197 @@
+import React from 'react';
+import {View, Text, TouchableOpacity, Image} from 'react-native';
+import {COLOURS} from '../database/items';
+import FontAwesome from 'react-native-vector-icons/FontAwesome5';
+import AntDesign from 'react-native-vector-icons/AntDesign';
+import Entypo from 'react-native-vector-icons/Entypo';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+
+const Details = ({route, navigation}) => {
+  const {
+    name,
+    image,
+    Type,
+    Skill,
+    Time,
+    isTopOfTheWeek,
+  } = route.params;
+
+  return (
+    <View
+      style={{
+        width: '100%',
+        height: '100%',
+        backgroundColor: COLOURS.white,
+        position: 'relative',
+      }}>
+      <View
+        style={{
+          padding: 20,
+          flexDirection: 'row',
+          justifyContent: 'space-between',
+          alignItems: 'center',
+        }}>
+        <TouchableOpacity
+          onPress={() => navigation.goBack()}
+          style={{
+            width: 40,
+            height: 40,
+            borderRadius: 10,
+            borderWidth: 1,
+            borderColor: COLOURS.lightGray,
+            justifyContent: 'center',
+            alignItems: 'center',
+          }}>
+          <FontAwesome
+            name="angle-left"
+            style={{
+              fontSize: 15,
+              color: COLOURS.black,
+            }}
+          />
+        </TouchableOpacity>
+        <View
+          style={{
+            width: 40,
+            height: 40,
+            borderRadius: 10,
+            backgroundColor: COLOURS.accent,
+            opacity: isTopOfTheWeek ? 1 : 0.5,
+            justifyContent: 'center',
+            alignItems: 'center',
+          }}>
+          <AntDesign name="star" style={{fontSize: 15, color: COLOURS.white}} />
+        </View>
+      </View>
+      <Text
+        style={{
+          fontSize: 38,
+          color: COLOURS.black,
+          fontWeight: '800',
+          paddingHorizontal: 20,
+          maxWidth: 310,
+        }}>
+        {name}
+      </Text>
+      <View
+        style={{
+          flexDirection: 'row',
+          alignItems: 'center',
+          paddingHorizontal: 20,
+        }}>
+        <View style={{paddingHorizontal: 20}}>
+          <View style={{paddingVertical: 20}}>
+            <Text
+              style={{
+                fontSize: 12,
+                color: COLOURS.black,
+                opacity: 0.5,
+              }}>
+              ประเภท
+            </Text>
+            <Text
+              style={{
+                fontSize: 18,
+                color: COLOURS.black,
+                fontWeight: '600',
+              }}>
+              <MaterialCommunityIcons name="stove" style={{fontSize: 18, color: COLOURS.black}} />
+              {Type}
+            </Text>
+          </View>
+          <View style={{paddingVertical: 20}}>
+            <Text
+              style={{
+                fontSize: 12,
+                color: COLOURS.black,
+                opacity: 0.5,
+              }}>
+              ความง่ายในการทำ
+            </Text>
+            <Text
+              style={{
+                fontSize: 18,
+                color: COLOURS.black,
+                fontWeight: '600',
+              }}>
+              <AntDesign name="star" style={{fontSize: 18, color: COLOURS.black}} />
+              {Skill}
+            </Text>
+          </View>
+          <View style={{paddingVertical: 20}}>
+            <Text
+              style={{
+                fontSize: 12,
+                color: COLOURS.black,
+                opacity: 0.5,
+              }}>
+              ใช้เวลาในการทำ
+            </Text>
+            <Text
+              style={{
+                fontSize: 18,
+                color: COLOURS.black,
+                fontWeight: '600',
+              }}>
+              <Ionicons name="timer" style={{fontSize: 18, color: COLOURS.black}} />
+              {Time} นาที
+            </Text>
+          </View>
+        </View>
+        <View
+          style={{
+            width: 380,
+            height: 380,
+          }}>
+          <Image
+            source={image}
+            style={{
+              width: '100%',
+              height: '100%',
+              resizeMode: 'contain',
+            }}
+          />
+        </View>
+      </View>
+      <View
+        style={{
+          position: 'absolute',
+          width: '100%',
+          bottom: 0,
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}>
+        <TouchableOpacity
+          onPress={() => navigation.goBack()}
+          style={{
+            width: '90%',
+            height: 80,
+            backgroundColor: COLOURS.accent,
+            borderTopRightRadius: 20,
+            borderTopLeftRadius: 20,
+            justifyContent: 'center',
+            alignItems: 'center',
+            flexDirection: 'row',
+          }}>
+          <Text
+            style={{
+              fontSize: 16,
+              fontWeight: 'bold',
+              color: COLOURS.black,
+              letterSpacing: 1,
+              marginRight: 10,
+            }}>
+            กลับสู่หน้าหลัก
+          </Text>
+          <Entypo
+            name="chevron-right"
+            style={{fontSize: 16, color: COLOURS.black}}
+          />
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
+};
+
+export default Details;
